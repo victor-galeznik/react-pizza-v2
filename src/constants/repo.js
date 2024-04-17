393b03fd7293ec18b24cd685cf4cd5d1b2816d12
Victor <mfzigzag@gmail.com>
2024-04-18 01:48:27 +0300

    Добавление базового URL при запуске приложения на GitHub Pages

---
+++ src/constants/repo.js
@@ -0,0 +1,1 @@
+export const REPO_NAME = 'react-pizza-v2';
--- src/index.js
+++ src/index.js
@@ -2,11 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
+import { REPO_NAME } from './constants/repo';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
-  <BrowserRouter>
+  <BrowserRouter basename={`/${REPO_NAME}/`}>
     <App />
   </BrowserRouter>,
 );
