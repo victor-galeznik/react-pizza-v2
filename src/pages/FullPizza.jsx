c292e9d3d5b3a9725c3bc3791fad9b096dbabf17
Victor <mfzigzag@gmail.com>
2024-04-20 22:32:16 +0300

    Перенаправление на главную страницу при ненаходе пиццы (useNavigate)

--- src/pages/FullPizza.jsx
+++ src/pages/FullPizza.jsx
@@ -1,10 +1,11 @@
 import React, { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
 import axios from 'axios';
 
 const FullPizza = () => {
   const [pizza, setPizza] = useState();
   const { id } = useParams();
+  const navigate = useNavigate();
 
   useEffect(() => {
     async function fetchPizza() {
@@ -15,6 +16,7 @@ const FullPizza = () => {
         setPizza(data);
       } catch (error) {
         alert('Ошибка при получении пиццы');
+        navigate('/');
       }
     }
 
