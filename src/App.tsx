0b5fc874c555166cc543146765075cbbf3d2abba
Victor <mfzigzag@gmail.com>
2024-04-21 02:45:25 +0300

    Подлключаем TypeScript. Типизируем компонент FullPizza

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,4 @@
-import React from 'react';
-
-import { Routes, Route, Outlet } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 
 import Home from './pages/Home';
 import Cart from './pages/Cart';
