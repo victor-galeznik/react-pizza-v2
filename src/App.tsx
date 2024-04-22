7eaedbeea14a48ba10f2c6ba22f26f6b5ccd659e
Victor <mfzigzag@gmail.com>
2024-04-22 23:04:27 +0300

    Lazy loading для компонетов Cart, FullPizza и NotFound

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,47 @@
+import React, { Suspense, lazy } from 'react';
 import { Routes, Route } from 'react-router-dom';
 
 import Home from './pages/Home';
-import Cart from './pages/Cart';
-import NotFound from './pages/NotFound';
-import FullPizza from './pages/FullPizza';
 import MainLayout from './layouts/MainLayout';
-
 import './scss/app.scss';
+
+const Cart = lazy(() => import(/* webpackChunkName: "Cart" */ './pages/Cart'));
+const FullPizza = lazy(
+  () => import(/* webpackChunkName: "FullPizza" */ './pages/FullPizza'),
+);
+const NotFound = lazy(
+  () => import(/* webpackChunkName: "NotFound" */ './pages/NotFound'),
+);
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<MainLayout />}>
         <Route path="" element={<Home />} />
-        <Route path="cart" element={<Cart />} />
-        <Route path="pizza/:id" element={<FullPizza />} />
-        <Route path="*" element={<NotFound />} />
+        <Route
+          path="cart"
+          element={
+            <Suspense fallback={<div>Идёт загрузка</div>}>
+              <Cart />
+            </Suspense>
+          }
+        />
+        <Route
+          path="pizza/:id"
+          element={
+            <Suspense fallback={<div>Идёт загрузка</div>}>
+              <FullPizza />
+            </Suspense>
+          }
+        />
+        <Route
+          path="*"
+          element={
+            <Suspense fallback={<div>Идёт загрузка</div>}>
+              <NotFound />
+            </Suspense>
+          }
+        />
       </Route>
     </Routes>
   );
