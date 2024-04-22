9681468742787b9040d39ef4a86731f42f346d09
Victor <mfzigzag@gmail.com>
2024-04-22 21:17:34 +0300

    Сохраняем пиццы в LocalStorage

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import { Link, useLocation } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import { selectCart } from '../redux/slices/cartSlice';
@@ -8,13 +8,21 @@ import logoSvg from '../assets/img/pizza-logo.svg';
 
 const Header: React.FC = () => {
   const { items, totalPrice } = useSelector(selectCart);
-
   const location = useLocation();
+  const isMounted = useRef(false);
 
   const totalCount = items.reduce(
     (sum: number, item: any) => sum + item.count,
     0,
   );
+
+  useEffect(() => {
+    if (isMounted.current) {
+      const json = JSON.stringify(items);
+      localStorage.setItem('cart', json);
+    }
+    isMounted.current = true;
+  }, [items]);
 
   return (
     <div className="header">
